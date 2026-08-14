a02aead89cf3253c2868f662b6abfbbde7d5dcfc
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-08-14 23:47:59 +0200

    Add gamepad navigation to tizen

--- tizen/src/main.tsx
+++ tizen/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { init } from '@noriginmedia/norigin-spatial-navigation';
 import { setClientInfo } from '@pelagica/core';
 import App from './App.tsx';
+import { initGamepadNavigation } from './lib/gamepad-navigation';
 import pkg from '../package.json' with { type: 'json' };
 
 import '@pelagica/core/i18n';
@@ -10,6 +11,7 @@ import './index.css';
 import './theme.css';
 
 init();
+initGamepadNavigation();
 setClientInfo({ name: 'Pelagica Tizen', version: pkg.version });
 
 createRoot(document.getElementById('root')!).render(
