0ec22203101ab47f856ec7f191bd4c38959e95de
KartoffelChipss <KartoffelChipss@users.noreply.github.com>
2026-08-16 18:58:50 +0000

    deploy: db2cd5170d32464ffab01e231e38da915955c7a2

---
+++ assets/ItemContextMenu-EBfsbThQ.js
@@ -0,0 +1,1 @@
+import{ur as e}from"./useMusicPlayback-C_Di_j_N.js";import{r as t,t as n}from"./MusicItemContextMenu-DT62_ifi.js";import{t as r}from"./GeneraItemContextMenu-DeXidJ5C.js";var i=e(),a=({item:e,children:a,playLink:o})=>t(e.Type)?(0,i.jsx)(n,{item:e,children:a}):(0,i.jsx)(r,{item:e,playLink:o,children:a});export{a as t};
